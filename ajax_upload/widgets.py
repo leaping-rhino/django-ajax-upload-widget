be5309e979dbad0401606eec17407f005d9070f6
Morné Chamberlain <morne@leaping-rhino.com>
2017-09-15 16:31:43 +0200

    Update with Python 3 compatibility.

--- ajax_upload/widgets.py
+++ ajax_upload/widgets.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 
 from ajax_upload.models import UploadedFile
 
@@ -21,7 +21,7 @@ class AjaxClearableFileInput(forms.ClearableFileInput):
     def render(self, name, value, attrs=None):
         attrs = attrs or {}
         if value:
-            filename = u'%s%s' % (settings.MEDIA_URL, value)
+            filename = '%s%s' % (settings.MEDIA_URL, value)
         else:
             filename = ''
         attrs.update({
@@ -45,7 +45,7 @@ class AjaxClearableFileInput(forms.ClearableFileInput):
             elif file_path.startswith(settings.MEDIA_URL):
                 # Strip and media url to determine the path relative to media url base
                 relative_path = file_path[len(settings.MEDIA_URL):]
-                relative_path = urllib2.unquote(relative_path.encode('utf8')).decode('utf8')
+                relative_path = urllib.parse.unquote(relative_path.encode('utf8')).decode('utf8')
                 try:
                     uploaded_file = UploadedFile.objects.get(file=relative_path)
                 except UploadedFile.DoesNotExist:
@@ -54,5 +54,5 @@ class AjaxClearableFileInput(forms.ClearableFileInput):
                 else:
                     return File(uploaded_file.file)
             else:
-                raise AjaxUploadException(u'%s %s' % (_('File path not allowed:'), file_path))
+                raise AjaxUploadException('%s %s' % (_('File path not allowed:'), file_path))
         return None
